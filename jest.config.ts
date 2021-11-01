21576c5ed841e02826e1a556de1ba09b0423e5bf
lucsimao <lucasimao64@gmail.com>
2021-11-01 13:56:06 -0300

    chore: adding jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
