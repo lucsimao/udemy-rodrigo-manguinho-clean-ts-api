93e1ced82e2027035cea8351af1786508cd8641d
lucsimao <lucasimao64@gmail.com>
2021-11-13 14:59:08 -0300

    refactor: update tests

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 
 jest.mock('bcrypt', () => ({
   async hash() {
-    return new Promise((resolve) => resolve('hashed_value'));
+    return Promise.resolve('hashed_value');
   },
 }));
 
@@ -23,11 +23,20 @@ describe('Bcrypt Adapter', () => {
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
   });
 
-  test('should creturn a hash on success', async () => {
+  test('should return a hash on success', async () => {
     const { sut } = makeSut();
-
     const result = await sut.encrypt('any_value');
 
     expect(result).toBe('hashed_value');
+  });
+
+  test('should throw when bcrypt throws', async () => {
+    const { sut } = makeSut();
+    const hashSpy = jest.spyOn(bcrypt, 'hash') as jest.Mock;
+    hashSpy.mockRejectedValueOnce(new Error(''));
+
+    const result = sut.encrypt('any_value');
+
+    expect(result).rejects.toThrow();
   });
 });
