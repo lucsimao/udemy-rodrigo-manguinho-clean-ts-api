22714ec2c9ace5efcd7b8436ddb2683436982894
lucsimao <lucasimao64@gmail.com>
2021-11-17 17:38:12 -0300

    refactor: group middleware interfaces in a single facade

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,6 @@
+import { bodyParser, contentType, cors } from '../middlewares';
+
 import { Express } from 'express';
-import { bodyParser } from '../middlewares/body-parser';
-import { contentType } from '../middlewares/content-type';
-import { cors } from '../middlewares/cors';
 
 export default (app: Express): void => {
   app.use(bodyParser);
