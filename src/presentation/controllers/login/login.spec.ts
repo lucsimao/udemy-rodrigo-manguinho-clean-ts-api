0e9b369b2f879ade811cf0848d38e2d739cff762
lucsimao <lucasimao64@gmail.com>
2021-11-24 21:39:37 -0300

    feat: ensure LoginController returns 400 if an invalid email is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,12 +1,32 @@
+import { EmailValidator, HttpRequest } from '../../protocols';
 import { ILogin, LoginController } from './login';
+import { InvalidParamError, MissingParamError } from '../../errors';
 
-import { HttpRequest } from '../../protocols';
-import { MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
 
+const makeEmailValidator = () => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+
+  return new EmailValidatorStub();
+};
+
+const makeFakeRequest = (): HttpRequest<ILogin> => {
+  return {
+    body: {
+      email: 'any_email',
+      password: 'any_password',
+    },
+  };
+};
+
 const makeSut = () => {
-  const sut = new LoginController();
-  return { sut };
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new LoginController(emailValidatorStub);
+  return { sut, emailValidatorStub };
 };
 
 describe('Login Controller', () => {
@@ -32,5 +52,23 @@ describe('Login Controller', () => {
 
     const httpResponse = await sut.handle(httpRequest as HttpRequest<ILogin>);
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
+  });
+
+  it('should call email validator with correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
+    const httpRequest = makeFakeRequest();
+
+    await sut.handle(httpRequest as HttpRequest<ILogin>);
+    expect(isValidSpy).toBeCalledWith(httpRequest.body.email);
+  });
+
+  it('should return 400 when an invalid email is provided', async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
+    const httpRequest = makeFakeRequest();
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')));
   });
 });
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,11 @@
-import { Controller, HttpRequest, HttpResponse } from '../../protocols';
+import {
+  Controller,
+  EmailValidator,
+  HttpRequest,
+  HttpResponse,
+} from '../../protocols';
+import { InvalidParamError, MissingParamError } from '../../errors';
 
-import { MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
 
 export interface ILogin {
@@ -9,13 +14,22 @@ export interface ILogin {
 }
 
 export class LoginController implements Controller {
+  constructor(private readonly emailValidator: EmailValidator) {}
+
   async handle(
     httpRequest: HttpRequest<ILogin>
   ): Promise<HttpResponse<unknown>> {
-    if (!httpRequest.body.email) {
+    const { email, password } = httpRequest.body;
+    if (!email) {
       return badRequest(new MissingParamError('email'));
     }
-
-    return badRequest(new MissingParamError('password'));
+    if (!password) {
+      return badRequest(new MissingParamError('password'));
+    }
+    const isValid = this.emailValidator.isValid(email);
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'));
+    }
+    return badRequest(new InvalidParamError('emai32432423l'));
   }
 }
