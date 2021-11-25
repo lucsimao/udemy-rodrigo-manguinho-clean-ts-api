3966c3aae03fa973181a09d7875fe1990f0a56e0
lucsimao <lucasimao64@gmail.com>
2021-11-24 21:31:15 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,17 +1,36 @@
-import { LoginController } from './login';
+import { ILogin, LoginController } from './login';
+
+import { HttpRequest } from '../../protocols';
 import { MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
 
+const makeSut = () => {
+  const sut = new LoginController();
+  return { sut };
+};
+
 describe('Login Controller', () => {
   it('should return 400 when no email is provided', async () => {
-    const sut = new LoginController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         password: 'any_password',
-      },
+      } as Partial<ILogin>,
     };
 
-    const httpResponse = await sut.handle(httpRequest);
+    const httpResponse = await sut.handle(httpRequest as HttpRequest<ILogin>);
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')));
+  });
+
+  it('should return 400 when no password is provided', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+      } as Partial<ILogin>,
+    };
+
+    const httpResponse = await sut.handle(httpRequest as HttpRequest<ILogin>);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
   });
 });
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -3,10 +3,19 @@ import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
 
+export interface ILogin {
+  email: string;
+  password: string;
+}
+
 export class LoginController implements Controller {
   async handle(
-    httpRequest: HttpRequest<unknown>
+    httpRequest: HttpRequest<ILogin>
   ): Promise<HttpResponse<unknown>> {
-    return badRequest(new MissingParamError('email'));
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'));
+    }
+
+    return badRequest(new MissingParamError('password'));
   }
 }
