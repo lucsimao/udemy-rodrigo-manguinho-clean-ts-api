1d54d6f7902865b50c4f9d07b926f4bbfdc9def6
lucsimao <lucasimao64@gmail.com>
2021-11-29 22:11:08 -0300

    feat: ensure token generator throw if update throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -4,6 +4,7 @@ import { DbAuthentication } from './db-authentication';
 import { HashComparer } from '../../protocols/criptography/hash-comparer';
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
 import { TokenGenerator } from '../../protocols/criptography/token-generator';
+import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository';
 
 const makeFakeAccount = (): AccountModel => {
   return {
@@ -44,20 +45,30 @@ const makeTokenGenerator = () => {
   return new TokenGeneratorStub();
 };
 
+const makeUpdateAccessTokenRepository = () => {
+  class UpdateAccessTokenRepositoryStub implements UpdateAccessTokenRepository {
+    async update(id: string, token: string): Promise<void> {}
+  }
+  return new UpdateAccessTokenRepositoryStub();
+};
+
 const makeSut = () => {
   const loadAccountByEmailRepository = makeLoadAccountByEmailRepositoryStub();
   const hashComparerStub = makeHashComparer();
   const tokenGeneratorStub = makeTokenGenerator();
+  const updateAccessTokenRepositoryStub = makeUpdateAccessTokenRepository();
   const sut = new DbAuthentication(
     loadAccountByEmailRepository,
     hashComparerStub,
-    tokenGeneratorStub
+    tokenGeneratorStub,
+    updateAccessTokenRepositoryStub
   );
   return {
     sut,
     loadAccountByEmailRepository,
     hashComparerStub,
     tokenGeneratorStub,
+    updateAccessTokenRepositoryStub,
   };
 };
 
@@ -131,6 +142,35 @@ describe('DbAuthentication Tests', () => {
     const { sut, tokenGeneratorStub } = makeSut();
     jest
       .spyOn(tokenGeneratorStub, 'generate')
+      .mockRejectedValueOnce(new Error());
+
+    const promise = sut.auth(makeFakeAuthentication());
+
+    await expect(promise).rejects.toThrow();
+  });
+
+  it('Should return null when LoadAccountByEmailRepository return false', async () => {
+    const { sut } = makeSut();
+
+    const accessToken = await sut.auth(makeFakeAuthentication());
+
+    expect(accessToken).toBe('any_token');
+  });
+
+  it('Should call UpdateAccessTokenRepository with correct values', async () => {
+    const { sut, updateAccessTokenRepositoryStub } = makeSut();
+
+    const updateSpy = jest.spyOn(updateAccessTokenRepositoryStub, 'update');
+
+    await sut.auth(makeFakeAuthentication());
+
+    expect(updateSpy).toHaveBeenCalledWith('any_id', 'any_token');
+  });
+
+  it('Should throw if TokenGenerator  throws', async () => {
+    const { sut, updateAccessTokenRepositoryStub } = makeSut();
+    jest
+      .spyOn(updateAccessTokenRepositoryStub, 'update')
       .mockRejectedValueOnce(new Error());
 
     const promise = sut.auth(makeFakeAuthentication());
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -6,12 +6,14 @@ import {
 import { HashComparer } from '../../protocols/criptography/hash-comparer';
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
 import { TokenGenerator } from '../../protocols/criptography/token-generator';
+import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository';
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
-    private readonly tokenGenerator: TokenGenerator
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
   async auth(authentication: AuthenticationModel): Promise<string | null> {
@@ -20,11 +22,15 @@ export class DbAuthentication implements Authentication {
     );
 
     if (account) {
-      await this.hashComparer.compare(
+      const isValid = await this.hashComparer.compare(
         authentication.password,
         account.password
       );
-      await this.tokenGenerator.generate(account.id);
+      if (isValid) {
+        const accessToken = await this.tokenGenerator.generate(account.id);
+        await this.updateAccessTokenRepository.update(account.id, accessToken);
+        return accessToken;
+      }
     }
 
     return null;
